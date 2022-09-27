92e31dc9a56c047ff963037a724053d7442e9722
Alexroma1927 <110851434+Alexroma1927@users.noreply.github.com>
2022-09-27 13:51:24 +0200

    Add files via upload

---
+++ milestone1.py
@@ -0,0 +1,35 @@
+def __init__(self, word_list, num_lives=5):
+
+      word = 'banana'
+      word_guessed  = ['_','_','_','_','_','_']
+      num_letters = 6
+      num_lives = 5
+      list_letters = 0
+      print ("The mystery word has " +  len(self.word) + " characters ")
+      print ('word_guessed')
+      word_list = my_list
+my_list = ('banana','orange','apple','pinapple','peech')
+
+
+import random
+
+word_list = my_list
+
+my_list = ('banana','orange','apple','pinapple','peech')
+
+print (word_list)  
+
+from random import choice
+random.choice('my_list')
+
+my_list = ('banana','orange','apple','pinapple','peech')
+print (choice(my_list))
+
+while True:
+          letter = input ('Enter a letter: ')
+          if len(letter) > 1:
+            print('please, enter just one letter')
+          elif len(letter) == 1:
+            break
+else:
+            print('please , enter a character')
